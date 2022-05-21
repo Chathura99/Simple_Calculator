1acfdc1a5f9b4fbd06581e5a5d93b09b623c11b5
Chathura <chathura.madura11@gmail.com>
2022-05-21 23:58:23 +0530

    change App component(button,keyboard)

--- App.js
+++ App.js
@@ -1,21 +1,38 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { useState } from 'react';
+import { SafeAreaView, StyleSheet, Switch, Text } from 'react-native';
+import { myColors } from './android/app/src/styles/Colors';
+import { ThemeContext } from './android/app/src/context/ThemeContext';
+import MyKeyboard from './android/app/src/components/MyKeyboard';
 
-export default function App() {
+
+
+function App() {
+  const [theme, setTheme] = useState('light');
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    // ThemeContext - value={theme} set as global
+    <ThemeContext.Provider value={theme}>
+      <SafeAreaView style={theme === 'light' ? styles.container : [styles.container, {backgroundColor: 'black'}]}>
+        <Switch 
+          value={theme == 'dark'}
+          onValueChange={() => setTheme(theme === 'light' ? 'dark' : 'light')}
+        />
+        <Text>To Dark</Text>
+        {/* <Text>To Light</Text> */}
+
+        
+        <MyKeyboard />
+      </SafeAreaView>
+    </ThemeContext.Provider>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: myColors.light,
     alignItems: 'center',
-    justifyContent: 'center',
+    justifyContent: 'flex-start',
   },
 });
+
+export default App;
