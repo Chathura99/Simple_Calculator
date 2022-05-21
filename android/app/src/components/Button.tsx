865c67765dfb36bba1d94e634404e343ba56d6d4
Chathura <chathura.madura11@gmail.com>
2022-05-21 23:56:28 +0530

    create button component

---
+++ android/app/src/components/Button.tsx
@@ -0,0 +1,41 @@
+import { useContext } from "react";
+import { TouchableOpacity, Text } from "react-native";
+import { ThemeContext } from "../context/ThemeContext";
+import { Styles } from "../styles/Styles";
+
+interface ButtonProps {
+    onPress: () => void;
+    title: string;
+    isPurple?: boolean;
+    isGray?: boolean;
+}
+//Reusable buttons
+export default function Button({ title, onPress, isPurple, isGray }: ButtonProps) {
+    const theme = useContext(ThemeContext);
+    return (
+        <TouchableOpacity 
+            style={
+                isPurple
+                ? Styles.btnPurple
+                : isGray 
+                ? Styles.btnGray 
+                : theme === "light" 
+                ? Styles.btnLight 
+                : Styles.btnDark
+            } 
+            //passing props
+            onPress={onPress}>
+            <Text 
+               style={
+                   isPurple || isGray 
+                   ? Styles.smallTextLight
+                   : theme === "dark" 
+                   ? Styles.smallTextLight 
+                   : Styles.smallTextDark 
+                }
+            >
+                {title}
+            </Text>
+        </TouchableOpacity>
+    );
+}
